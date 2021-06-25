40a72821b922bf0195ca65eb0feb13abd2b5c430
shenyanyanya <vanya.toryanik@yandex.ru>
2021-06-25 15:06:45 +0300

    Structure Graph added to header file

--- graph.h
+++ graph.h
@@ -4,6 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Graph{
+    int V;
+    int **A;
+};
+
 struct Graph *create_graph(int V);
 void create_edge(struct Graph *G, int src, int dst, int wt);
 void delete_edge(struct Graph *G, int src, int dst);
